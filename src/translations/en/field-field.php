a7144629b368a741a4a9581085be316dca3d070c
Thomas Templeton <thomas.templeton.dev@gmail.com>
2023-06-16 13:55:31 +1000

    Default translations file

---
+++ src/translations/en/field-field.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Allowed Field Groups' => 'Allowed Field Groups',
+    'Fields' => 'Fields',
+    'Which field groups a selectable field must belong to.' => 'Which field groups a selectable field must belong to.',
+];
